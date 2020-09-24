b7d601a812e4d1e68ab6302f126413ac9dda543d
John Steve Palero <jtpalero.halcyondigital@gmail.com>
2020-09-24 17:59:04 +0800

    chore: removed eslint comment

--- src/redux/news/action-types.ts
+++ src/redux/news/action-types.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export const FETCH_NEWS_REQUEST = "FETCH_NEWS_REQUEST"
 export const FETCH_NEWS_SUCCESS = "FETCH_NEWS_SUCCESS"
 export const FETCH_NEWS_FAILED = "FETCH_NEWS_FAILED"
